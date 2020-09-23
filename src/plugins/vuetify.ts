ddfa66c4ba94e1f8558a97607e31bde2bda6bb40
Matthias Schwarz <70815012+matthiasschwarz@users.noreply.github.com>
2020-09-23 17:54:23 +0200

    Initial theme based on system theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,10 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark:
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+  }
+});
